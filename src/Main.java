6e0d0a11086be0b7ced6234d14caa486b4e6272d
Dogggy9 <dogggy9@gmail.com>
2024-05-18 15:24:15 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
+    public static String secret = "AmIgo";
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.nextLine();
+
+        if (word.equalsIgnoreCase(secret))
+            System.out.println("разрешен");
+        else
+            System.out.println("запрещен");
     }
 }
